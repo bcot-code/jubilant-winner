c7f6f9f49365521209a9da937584725f379b3dc6
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-02 17:50:34 -0600

    adding a comment, delete, update

--- public/js/add_post.js
+++ public/js/add_post.js
@@ -1,3 +1,22 @@
 const add_post = async () => {
-    
-}
+  let title = document.querySelector("#title").value;
+  let content = document.querySelector("#content").value;
+  if (title === "" || content === "") {
+    alert("Please fill out all fields");
+  } else {
+    await fetch("/api/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: title, content: content }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success!", data);
+        window.location.replace("/dashboard");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+};
+document.getElementById("addPostBtn").addEventListener("click", add_post);
