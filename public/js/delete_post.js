5d21708c162dd45c9297d6c3807498822b3ce219
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-02 19:36:49 -0600

    adding the delete post

--- public/js/delete_post.js
+++ public/js/delete_post.js
@@ -1,3 +1,19 @@
-const deletepost = async () => {
-  //   let postId = document.querySelector("#deletePost").dataset.id;
+const deletepost = async (event) => {
+  // console.log("delete button clicked");
+  event.preventDefault();
+  let post_id = window.location.toString().split("/").pop();
+  if (post_id) {
+    const response = await fetch(`/api/posts/${post_id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to delete project");
+    }
+  } else {
+    alert("No Post ID found!");
+  }
 };
+// Listen for a click on the delete button, and run the deletePost function
+document.querySelector(".delBtn").addEventListener("click", deletepost);
